0f7c44ea5c6c93546bac709e371a1f20b4c1ddfa
Alex Farmer <alex@Shawnas-MacBook-Air.local>
2022-09-19 14:21:59 -0400

    Updates

---
+++ precache-manifest.9439796a3cf47af379db17656f577a7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "151e5b25897b60a7135a",
+    "url": "/visual-sorter/static/css/main.80bda5f0.chunk.css"
+  },
+  {
+    "revision": "151e5b25897b60a7135a",
+    "url": "/visual-sorter/static/js/main.e592226d.chunk.js"
+  },
+  {
+    "revision": "88425b4bc8181cf78bfe",
+    "url": "/visual-sorter/static/js/runtime~main.2b99e081.js"
+  },
+  {
+    "revision": "d83874a9fb3344192d7d",
+    "url": "/visual-sorter/static/js/2.962529df.chunk.js"
+  },
+  {
+    "revision": "79be80b375553eb66abb",
+    "url": "/visual-sorter/static/js/3.c70b6b5b.chunk.js"
+  },
+  {
+    "revision": "c5b0f4489dd2cd52eacc1a0e156ec5c3",
+    "url": "/visual-sorter/index.html"
+  }
+];
